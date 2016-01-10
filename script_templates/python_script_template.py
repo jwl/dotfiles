07009763bfbeb7af909cb0551d1c9c6ab9ac7675
jwl <james.warren.leung@gmail.com>
2016-01-10 02:39:06 -0700

    Numerous edits and additions.
    * Changed nvimrc to only highlight current line in the currently active pane.
    * Changed windows vimrc to use vim-plug instead of vundle
    * Changed windows vimrc to match settings and behavior of nvimrc
    * added a script_templates folder to store templates of commonly used scripts
    * added a blank python script template to be used in creating quick
    simple command line utilities

---
+++ script_templates/python_script_template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+
+    # if not args:
+        # print('usage: [--flags options] [inputs]')
+        # sys.exit(1)
+
+
+
+if __name__ == '__main__':
+    # call functions here
+    main()
