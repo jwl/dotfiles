1b4c3e65e5892ea3f6db2f5c8c6327506699c94e
James Leung <jleung@riotgames.com>
2025-05-08 10:30:13 -0700

    slight change to work wezterm config

--- work_wezterm_config.lua
+++ work_wezterm_config.lua
@@ -3,10 +3,15 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local action = wezterm.action
 
 -- Appearance options
 config.color_scheme = 'Gruvbox (Gogh)'
-config.font = wezterm.font 'JetBrains Mono'
+-- config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+  weight = 'Medium',
+}
 config.enable_tab_bar = false
 
 
