3003a2ebd423487ee43f3b1d4e888d0f891d3b93
James Leung <james.warren.leung@pm.me>
2025-05-08 10:29:45 -0700

    adding home wezterm config

---
+++ carroway_wezterm_config.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+local action = wezterm.action
+
+-- Appearance options
+config.color_scheme = 'Gruvbox light, hard (base16)'
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+  weight = 'Medium',
+}
+config.enable_tab_bar = false
+
+
+-- and finally, return the configuration to wezterm
+return config
